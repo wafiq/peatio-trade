e8ab1b95745a82c7701bf8fa0a79a0f2bfb9c04c
Wafiq Rodzuan <wafiqrodzuan@gmail.com>
2018-04-16 20:01:54 +0700

    Add audio assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,7 +6,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.prefix = '/trading-ui-assets'
 
 # Add additional assets to the asset load path.
-# Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.paths << Rails.root.join("app", "assets", "audio")
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
